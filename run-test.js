798e29964bcbc27eb5d4a6a280c833044adf686b
Heather Arthur <fayearthur@gmail.com>
2014-04-03 16:25:11 -0700

    print basic stats

--- run-test.js
+++ run-test.js
@@ -1,4 +1,49 @@
-var fs = require("fs");
+var fs = require("fs"),
+    path = require("path");
 
-var gcd = require("algos/gcd");
+var gcd = require("./algos/gcd");
 
+var dir = "files";
+var langs = ["HTML", "CSS", "JavaScript"];
+
+
+printStats();
+
+function printStats() {
+  var stats = getStats();
+  for (var lang in stats) {
+    console.log(lang);
+    var count = stats[lang];
+    for (var width in count) {
+      console.log(" " + width + ": " + count[width]);
+    }
+  }
+}
+
+function getStats() {
+  var counts = {};
+  for (var i in langs) {
+    var lang = langs[i];
+    counts[lang] = {};
+
+    var files = fs.readdirSync(path.join(dir, lang));
+    for (var j in files) {
+      var indent = getIndent(files[j]);
+      counts[lang][indent] = (counts[lang][indent] || 0) + 1;
+    }
+  }
+  return counts;
+}
+
+function getIndent(filename) {
+  if (/-tabs\./.test(filename)) {
+    return "tabs";
+  }
+
+  var matches = filename.match(/-(\d)\./);
+  if (!matches) {
+    return 0;
+  }
+  var spaces = parseInt(matches[1] || "0");
+  return spaces;
+}
