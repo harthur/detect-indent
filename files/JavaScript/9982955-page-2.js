5cff2ee260359778f26ad5ca71e2b0b6df1ac6f2
Heather Arthur <fayearthur@gmail.com>
2014-04-04 14:53:13 -0700

    delete some one-liner files

--- files/JavaScript/9982955-page-2.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (nodeOrSelector) {
-  require('common');
-  // use "jquery" supplied by "common"
-  var $ = require("jquery");
-  // do page stuff with $ or other modules from "common"
-};
